3ad072a2b91c3cf1d1b91ce8ada1008ccc2e1b8f
Tomasz Kaminski <tomasz77ka@gmail.com>
2023-05-04 20:45:06 +0200

    średniki działają

--- Bricks.java
+++ Bricks.java
@@ -6,47 +6,38 @@ public class Bricks {
     static int bricksUsedInStageII = 0;
     static int bricksLeftInBox = 0;
     static int bricksMissingInInstructions = 0;
-    static int buildingsBuilt = 0;
-    static int buildingsNotBuilt = 0;
-
     static Set<Instruction> setOfInstructions = new HashSet<>();
     static List<Integer> numbersUsedInInstruction = new ArrayList<>();
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in).useDelimiter("\\s+");
-        while (input.hasNext()) {
-            String i = input.nextLine();
-            SegmentAnalizer(i);
-        }
-
+        readFile();
         BuildingProcess();
-
-
-        System.out.println(bricksUsedInStageI);
-        System.out.println(bricksUsedInStageII);
-        System.out.println(bricksLeftInBox);
-        System.out.println(bricksMissingInInstructions); //
-        System.out.println(contOfBuildingsPossible()[0]);
-        System.out.println(contOfBuildingsPossible()[1]);
-
-
+        printOutput();
     }
 
-    //ta funkcja zapisuje dane
-    public static void SegmentAnalizer(String input) {
-        //sprawdzenie ile cyfr ma liczba ktora reprezentuje instrukcje
+    private static void readFile() {
+        Scanner input = new Scanner(System.in).useDelimiter(";\\s*");
+        while (input.hasNext()) {
+            String i = input.next();
+            segmentAnalyzer(i);
+        }
+    }
+
+
+    public static void segmentAnalyzer(String input) {
         int colonIndex = input.indexOf(':');
         if (colonIndex == -1) {
             System.out.println("klops");
+            System.exit(0);
             return;
         }
 
         int number = Integer.parseInt(input.substring(0, colonIndex));
-        String BricksLetters = input.substring(colonIndex + 1);
+        String bricksLetters = input.substring(colonIndex + 1);
 
         if (number == 0) {
             bricksLeftInBox++;
-            Box.getInstance().addBrick(BricksLetters);
+            Box.getInstance().addBrick(bricksLetters);
 
         } else if (number > 0 && number <= 1000) {
             //jesli jest juz utworzona dana instrukcja to dodaje do niej nowe klocki, jesli
@@ -60,29 +51,29 @@ public class Bricks {
                 }
             }
 
+
             if (CreateNewList) {
                 Instruction instruction = new Instruction(number);
-                instruction.addBricks(BricksLetters);
+                instruction.addBricks(bricksLetters);
                 setOfInstructions.add(instruction);
                 numbersUsedInInstruction.add(number);
             } else {
                 for (Instruction instruction : setOfInstructions) {
                     if (instruction.getId() == number) {
-                        instruction.addBricks(BricksLetters);
+                        instruction.addBricks(bricksLetters);
                         break;
                     }
                 }
             }
         } else {
             System.out.println("klops");
+            System.exit(0);
         }
     }
-
     public static void BuildingProcess() {
         for (Instruction instruction : setOfInstructions) {
             if (instruction.getId() % 3 == 0) {
 
-                System.out.println("sprawdzam kocki dla instrukcji numer " + instruction.getId() + "\n");
 
                 bricksUsedInStageI += build(instruction);
 
@@ -91,7 +82,6 @@ public class Bricks {
         for (Instruction instruction : setOfInstructions) {
             if (!(instruction.getId() % 3 == 0)) {
 
-                System.out.println("sprawdzam kocki dla instrukcji numer " + instruction.getId() + "\n");
 
                 bricksUsedInStageII += build(instruction);
 
@@ -111,33 +101,27 @@ public class Bricks {
             boolean brickFound = false;
             for (int i = 0; i <= box.index; i++) {
                 if (box.getBrickArray()[i] != null && box.getBrickArray()[i].equals(brickInInstruction)) {
-                    System.out.println("został znaleziony klocek " + brickInInstruction + "\n");
                     brickFound = true;
                     listOfIndexOfUsedBricks.add(i);
-                    box.getBrickArray()[i]=null;
-
-
-
-                    //TODO TUTAJ MOZE BYC PROBLEM Z TYM ZE JAK INSTRUKCJA SIE OKAZE NIEWYKONALNA TO I ATK POLICZY KLOCKI JAKO UZYTE
+                    box.getBrickArray()[i] = null;
                     break;
                 }
             }
             if (!brickFound) {
                 allBricksFound = false;
-                System.out.println("nie został znaleniony klocek" + brickInInstruction + " dla budowli " + instruction.getId() + "\n");
                 break;
             }
         }
         instruction.itIsPossibleToBuild = allBricksFound;
         if (allBricksFound) {
 
-            bricksLeftInBox-=instruction.getIndex();
+            bricksLeftInBox -= instruction.getIndex();
 
             return 1;
-        }else {
+        } else {
 
 
-            bricksMissingInInstructions +=(instruction.getIndex()- listOfIndexOfUsedBricks.size());
+            bricksMissingInInstructions += (instruction.getIndex() - listOfIndexOfUsedBricks.size());
         }
 
         return 0;
@@ -152,5 +136,14 @@ public class Bricks {
             } else counterNotPossible++;
         }
         return new int[]{counterPossible, counterNotPossible};
+    }
+
+    private static void printOutput() {
+        System.out.println(bricksUsedInStageI);
+        System.out.println(bricksUsedInStageII);
+        System.out.println(bricksLeftInBox);
+        System.out.println(bricksMissingInInstructions);
+        System.out.println(contOfBuildingsPossible()[0]);
+        System.out.println(contOfBuildingsPossible()[1]);
     }
 }
